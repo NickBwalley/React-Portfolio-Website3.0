e75e1d5121d56d82208761667264283c7a5a82fe
NickBwalley <nickbwalley@gmail.com>
2022-12-22 11:24:05 +0300

    material UI icons with React

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,4 +1,5 @@
 import "./topbar.scss";
+import { Person, Mail } from "@material-ui/icons";
 
 export default function Topbar() {
   return (
@@ -8,6 +9,14 @@ export default function Topbar() {
           <a href="#intro" className="logo">
             Qonixs.
           </a>
+          <div className="itemContainer">
+            <Person className="icon" />
+            <span>+254 714 394 332</span>
+          </div>
+          <div className="itemContainer">
+            <Mail className="icon" />
+            <span>nickbwalley@gmail.com</span>
+          </div>
         </div>
         <div className="right"></div>
       </div>
